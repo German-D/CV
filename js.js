76f849bbf7983e4b9f935c9dbe856dfefeabde2e
German Dolnikov <german@dolnikov.ru>
2022-10-13 18:59:33 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 /* Это объявление переменной, мы наши кнопку по тегу */
-const button = document.querySelector('button');
+var button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
