97158c003377f16346721c102038a881abad3b1c
German Dolnikov <german@dolnikov.ru>
2022-10-27 22:29:21 +0300

    изменил девиз

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ var button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	alert('Мур мур')
+	alert('Деньги это не главное')
 })
