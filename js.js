415cd820670af5daeb8b63949d25852e962e9eda
German Dolnikov <german@dolnikov.ru>
2023-08-17 20:03:07 +0300

    Update js.js

--- js.js
+++ js.js
@@ -3,5 +3,6 @@ var button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
+	console.log('log: Выведи этот текст в консоль если кнопка нажмётся');
 	alert('Деньги это не главное')
 })
